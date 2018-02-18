d48e6505c4ed09187b6cee45c1743814710813b2
falsabhan <falsabhan@drury.edu>
2018-02-17 20:05:05 -0600

    builded another view XIB to fullly control the callout
    - Created a view XIB file: it contains a tree name, image holder, subtitle and a tree description
    - created another class that inherents from the UIView which has all of the functionality to use to build the custom view and connected the view to the class.

---
+++ D.Eco/codeBase/D.Eco/D.Eco/CustomAnnotationCallout.swift
@@ -0,0 +1,29 @@
+//
+//  CustomAnnotationCallout.swift
+//  D.Eco
+//
+//  Created by Fisal Alsabhan on 2/17/18.
+//  Copyright © 2018 Fisal Alsabhan. All rights reserved.
+//
+
+import UIKit
+
+class CustomAnnotationCallout: UIView {
+
+    @IBOutlet weak var imageHolder: UIImageView!
+    @IBOutlet weak var treeName: UILabel!
+    @IBOutlet weak var treeSubtitle: UILabel!
+    @IBOutlet weak var treeDescription: UILabel!
+    @IBAction func DetailViewSegueButton(_ sender: Any) {
+        
+        // preform action here
+    }
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
